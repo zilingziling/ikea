823a04ab06eaa46b9cc052f69be14eeebabfb92d
jianglongyan <longyan.jiang_tic@baozun.com>
2020-02-27 11:56:15 +0800

    one

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,68 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/LOGO.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+<view class="ikea">
+	<div class="logo">
+		<img src="/static/LOGO.png" alt />
+	</div>
+	<div class="jion">
+		<b>欢迎加入</b>
+		<b>宜家会员俱乐部</b>
+	</div>
+	<div class="return">
+		<div class="return-btn"><a @click="backMemberCenter">返回会员中心</a><uni-icons type="contact" size="30"></uni-icons></div>
+	</div>
+</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import uniIcons from "../../../components/yangxiaochuang-icons/yangxiaochuang-icons"
+export default {
+components: {uniIcons},
+data() {
+	return {};
+},
+onLoad() {},
+methods: {}
+};
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
+.logo {
+	width: 100%;
+	height: 270rpx;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #0077c8;
 	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
+img {
+	width: 213rpx;
+	height: 90rpx;
 	}
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+.jion {
+	width: 100%;
+	height: 220rpx;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	}
+	
+b {
+	display: block;
+	font-size: 60rpx;
+	color: #0077c8;
+	}
+.return{
+	width: 100%;
+	display: flex;
+	align-items: flex-end;
+	justify-content: center;
+	position: absolute;
+	bottom: 60rpx;
+	color:#8f8f8f;
+}
+
+
 </style>
---
+++ src/pages/moreverify/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="moreverify">
+        moreerify
+    </div>
+</template>
